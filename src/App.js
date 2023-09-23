7780729da703e6f76c912c16f991c867cbdf73b2
Uzair Oomer <uzair.oomer@jdplc.com>
2023-09-23 16:17:37 +0100

    Implement Navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   return (
-    <div>Hello World</div>
+    <Navbar />
   )
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className='app__navbar'>
+        <div className="app__navbar-logo">
+            The Grub Cabin
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
