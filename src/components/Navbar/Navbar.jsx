b79c71bc6f36b1b0938924cba8aa23247877dc8e
Uzair Oomer <uzair.oomer@jdplc.com>
2023-09-24 14:16:45 +0100

    Completed the navbar jsx and css files

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css'
 import { RxHamburgerMenu } from 'react-icons/rx';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <nav className='app__navbar'>
         <div className="app__navbar-title">
@@ -20,13 +23,19 @@ const Navbar = () => {
             <a href="/">Book a Table</a>
         </div>
         <div className="app__navbar-smallscreen">
-            <RxHamburgerMenu color='white' fontSize={27}/>
-            <ul className='app__navbar-smallscreen-links'>
-            <li className='navbar-links'><a href="#home">Home</a></li>
-            <li className='navbar-links'><a href="#about">About</a></li>
-            <li className='navbar-links'><a href="#home">Menu</a></li>
-            <li className='navbar-links'><a href="#contact">Contact</a></li>
-        </ul>
+            <RxHamburgerMenu color='white' fontSize={27} onClick={() => setToggleMenu(true)} />
+
+            {toggleMenu &&(
+                <div className="app__navbar-smallscreen_overlay flex__center slider-bottom">
+                <MdOutlineRestaurantMenu className='overlay__close' fontSize={27} onClick={() => setToggleMenu(false)} />
+                <ul className='app__navbar-smallscreen_links'>
+                    <li className='navbar-links'><a href="#home">Home</a></li>
+                    <li className='navbar-links'><a href="#about">About</a></li>
+                    <li className='navbar-links'><a href="#home">Menu</a></li>
+                    <li className='navbar-links'><a href="#contact">Contact</a></li>
+                </ul>
+                </div>
+            )}
         </div>
     </nav>
   )
