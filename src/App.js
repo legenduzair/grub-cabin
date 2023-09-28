0c0a44bf28491c8d622e12e17bc46bf0d77125b1
Uzair Oomer <uzair.oomer@jdplc.com>
2023-09-28 15:16:02 +0100

    Add about us component and styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
+import AboutUs from './components/AboutUs/AboutUs'
 import './App.css'
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <>
       <Navbar />
       <Header />
+      <AboutUs />
     </>
   )
 }
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,5 +1,7 @@
 import header from '../assets/header.jpg'
+import knife from '../assets/knife.png';
 
 export default {
     header,
+    knife,
 };
